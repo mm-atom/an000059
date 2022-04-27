cba947bdb1f747c6b7b6313fdfdcde5e02963e4b
taoqf <tao_qiufeng@126.com>
2022-04-27 17:30:16 +0800

    init

--- tests/test.js
+++ tests/test.js
@@ -2,6 +2,13 @@ const test = require('ava');
 
 const { default: a } = require('../dist/index');
 
-test('xxx', (t) => {
-	t.pass();
+test('remove undefined value', (t) => {
+	const filter = {
+		foo: 'bar',
+		fd: undefined
+	};
+	const validateFilter = a(filter);
+	t.deepEqual(validateFilter, {
+		foo: 'bar'
+	});
 });
