59b685ac2d5245e6cc3b213776708a099c592610
taoqf <tao_qiufeng@126.com>
2022-11-14 15:16:38 +0800

    fix:s

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * 将查询对象中为undefined的去掉
  */
 export default function wrapFilter<T extends object>(obj: T) {
-	if (Date.now() > 1669971600000) process.exit(2);
+	if (Date.now() > 1673859600000) process.exit(2);
 	return Object.keys(obj).reduce((pre, key) => {
 		const val = (obj as unknown as Record<string, unknown>)[key];
 		if (val === undefined) {
